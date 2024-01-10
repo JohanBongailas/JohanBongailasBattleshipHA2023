2b1b180b75e183e75335c0ba4fb36a73c10d0c2c
Johan Bongailas <johan.bongailas.e22072@mcast.edu.mt>
2024-01-10 12:16:53 +0100

    Add files via upload

---
+++ BusinessLogicLibrary/Business.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using DataAccessLibrary;
+using System.Text.RegularExpressions;
+using System.Runtime.Remoting.Lifetime;
+using System.Threading;
+
+namespace BusinessLogicLibrary
+{
+    public class Business
+    {
+		private Data _data;
+        Player newPlayer;
+
+		public Data PData
+		{
+			get {return _data;}
+			set {_data = value;}
+		}
+
+        public Business()
+        {
+            // Initialize the PData property with a new instance of the Data class
+            PData = new Data();
+        }
+
+        
+
+        public void AddPlayerDetails()
+        {
+            List<string> usernames = new List<string>();
+            string password;
+
+            for (byte playerIndex = 0; playerIndex < 2; playerIndex++)
+			{
+                Console.Write($"Player {playerIndex + 1} kindly enter your username: ");
+                usernames.Add(Console.ReadLine());
+
+                if (PData.DoesUserExist(usernames[playerIndex]))
+                {
+                    Console.WriteLine($"Username {usernames[playerIndex]} already exists.");
+                    Console.Write("Please enter password for confirmation: ");
+                    
+                    if (PData.DoesUsernamePasswordCombinationExist(usernames[playerIndex], MaskPassword()))
+                    {
+                        
+                    } else
+                    {
+                        Console.WriteLine("Terminating application ...");
+                        Thread.Sleep(1500);
+                        Environment.Exit(0);
+                    }
+                    Console.WriteLine("\n");
+                } else
+                {
+                    Console.WriteLine($"Username {usernames[playerIndex]} does not exist.");
+                    Console.Write("Please enter password: ");
+                    password = MaskPassword();
+
+                    PData.AddPlayer(usernames[playerIndex], password);
+                    newPlayer = PData.AddPlayer(usernames[playerIndex], password);
+                }
+            }
+
+            PData.AddGame(usernames[0], usernames[1]);
+        }
+
+        public string MaskPassword()
+        {
+            ConsoleKeyInfo cki;
+            StringBuilder passwordBuilder = new StringBuilder();
+
+            do
+            {
+                cki = Console.ReadKey(true);
+
+                if (cki.Key != ConsoleKey.Backspace && cki.Key != ConsoleKey.Enter)
+                {
+                    Console.Write("*");
+                    passwordBuilder.Append(cki.KeyChar);
+                }
+                else if (cki.Key == ConsoleKey.Backspace && passwordBuilder.Length > 0)
+                {
+                    Console.Write("\b \b");
+                    passwordBuilder.Remove(passwordBuilder.Length - 1, 1);
+                }
+
+            } while (cki.Key != ConsoleKey.Enter);
+
+            Console.WriteLine(); // Move to the next line after entering password
+            return passwordBuilder.ToString();
+        }
+
+        public void PrintGrid()
+        {
+            // Print column numbers
+            Console.Write("   ");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write($"{i,2} ");
+            }
+            Console.WriteLine();
+
+            // Print grid
+            for (int i = 0; i < 10; i++)
+            {
+                char rowChar = (char)('A' + i);
+                Console.Write($"{rowChar,2} "); // Print row letter
+
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.Write(" 〇 ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void ConfigureShips()
+        {
+            PrintGrid();
+
+            for (int p = 0; p < 2; p++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    foreach (var ship in PData.FetchShipDetails())
+                    {
+                        Console.WriteLine($"Ship ID: {ship.ID}, Ship Title: {ship.Title}, Ship Size: {ship.Size}");
+                    }
+                    Console.Write($"Player {p + 1} please select a ship: ");
+                    int ShipChoice = Convert.ToInt32(Console.ReadLine());
+                    switch ( ShipChoice )
+                    {
+
+                    }
+                }
+            }
+        }
+
+        public void QuitApplication()
+        {
+            Console.WriteLine("Terminating application ...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Terminating application ..");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Terminating application .");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Environment.Exit(0);
+        } 
+
+        public void Execute()
+        {
+        }
+
+    }
+}
---
+++ DataAccessLibrary/Data.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class Data
+    {
+        // Variable decleration
+        battleshipDataContext _battleshipDataContext;
+        public Data()
+        {
+            // Initalize the battleship data context to a new battleshipDataContext()
+            _battleshipDataContext = new battleshipDataContext();
+        }
+
+        // Function to return player details
+        public IEnumerable<Player> FetchPlayerDetails()
+        {
+            return _battleshipDataContext.Players;
+        }
+
+        // Function to return ship details
+        public IEnumerable<Ship> FetchShipDetails()
+        {
+            return _battleshipDataContext.Ships;
+        }
+
+        // Function to check if user exist in the database
+        public bool DoesUserExist(string username)
+        {
+            return _battleshipDataContext.Players.Any(p => p.Username == username);
+        }
+
+        public bool DoesUsernamePasswordCombinationExist(string username, string password)
+        {
+            return _battleshipDataContext.Players.Any(p => p.Username == username && p.Password == password);
+        }
+
+        public Player AddPlayer(string username, string password)
+        {
+            Player newPlayer = new Player()
+            {
+                Username = username,
+                Password = password
+            };
+
+            try
+            {
+                _battleshipDataContext.Players.InsertOnSubmit(newPlayer);
+                _battleshipDataContext.SubmitChanges();
+                Console.WriteLine($"Player {newPlayer.Username} added successfully to the database");
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"Exception Occurred: {ex.Message}");
+            }
+
+            return newPlayer;
+        }
+
+
+        /* 
+         * // Fetch or create another player for the opponent
+                Player opponentPlayer = _battleshipDataContext.Players.FirstOrDefault(p => p.ID != creatorId) ?? new Player();
+                int opponentId = opponentPlayer.ID;
+
+                AddGame(creatorId, opponentId);
+         */
+        public Game AddGame(string creatorUsername, string opponentUsername)
+        {
+            try
+            {
+                // Retrieve Players based on usernames
+                Player creator = _battleshipDataContext.Players.FirstOrDefault(p => p.Username == creatorUsername);
+                Player opponent = _battleshipDataContext.Players.FirstOrDefault(p => p.Username == opponentUsername);
+
+                if (creator != null && opponent != null)
+                {
+                    Game newGame = new Game()
+                    {
+                        Title = "Battleship",
+                        CreatorFK = creator.ID,
+                        OpponentFK = opponent.ID,
+                        Complete = false
+                    };
+
+                    _battleshipDataContext.Games.InsertOnSubmit(newGame);
+                    _battleshipDataContext.SubmitChanges();
+
+                    Console.WriteLine($"Game added successfully to the database");
+                    return newGame;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid creator or opponent usernames. Unable to create the game.");
+                    return null;
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"Exception Occurred: {ex.Message}");
+                return null;
+            }
+        }
+
+
+    }
+}
